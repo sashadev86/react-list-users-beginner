83d4e2b25f317db5ffa6cd64a31ec59b65a8263e
Alexsandr Valentinov <djvalentinoff@gmail.com>
2024-10-05 14:09:54 +0300

    feat: add search and send invite

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
 import React from "react";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "./index.scss";
-// import { Success } from "./components/Success";
+import { Success } from "./components/Success";
 import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [invites, setInvites] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     fetch("https://reqres.in/api/users")
       .then((res) => res.json())
       .then((json) => {
         setUsers(json.data);
-      }).catch((err) => {
+      })
+      .catch((err) => {
         console.warn(err);
-        alert("Ошибка при получении пользователей")
-      }).finally(() => setLoading(false));
+        alert("Ошибка при получении пользователей");
+      })
+      .finally(() => setLoading(false));
   }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value); // Получаем значение поиска
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setInvites((prev) => prev.filter((item) => item !== id));
+    } else {
+      setInvites((prev) => [...prev, id]); // Добавляем id в массив
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);   
+  };
 
   return (
     <div className="App">
-      <Users items={users} isLoading={isLoading} />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
